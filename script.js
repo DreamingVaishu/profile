4ecc3478b4a0e751b8198ea48399a17d4fcada02
DreamingVaishu <daydreamingvaishu@gmail.com>
2023-12-05 22:38:09 +0530

    stabilize

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 console.log(window.innerWidth);
 if(window.innerWidth <= 750) {
     console.log("yess it is!");
+    document.getElementById('navbar').style.width = "100%";
     document.getElementById('profile').style.width = "100%";
-    document.getElementById('colom').style.fontSize = "15px";
-    document.getElementById('colom1').style.fontSize = "15px";
-    document.getElementById('colom2').style.fontSize = "15px";
-    document.getElementById('colom3').style.fontSize = "15px";
-    document.getElementById('colom4').style.fontSize = "15px";
     document.getElementById('box').style.clipPath = "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)";
-    // document.getElementById('box').style.justifyContent = "center";
-    // document.getElementById('box').style.textAlign = "center";
     document.getElementById('card').style.justifyContent = "center";
 }
 
